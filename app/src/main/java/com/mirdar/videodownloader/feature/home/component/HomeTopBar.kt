abcc67461606162f79a4e39d02e47c210e1fac78
Mohammad Mirdar <mohammadmirdarmpanahi@gmail.com>
2025-09-19 17:54:53 +0330

    Fix home top bar sizes

--- app/src/main/java/com/mirdar/videodownloader/feature/home/component/HomeTopBar.kt
+++ app/src/main/java/com/mirdar/videodownloader/feature/home/component/HomeTopBar.kt
@@ -1,6 +1,9 @@
 package com.mirdar.videodownloader.feature.home.component
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -12,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.mirdar.designsystem.theme.VideoDownloaderTheme
-import com.mirdar.videodownloader.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,14 +25,16 @@ internal fun HomeTopBar(modifier: Modifier = Modifier) {
         ), title = {
             Text(
                 "Video Downloader",
-                style = MaterialTheme.typography.headlineSmall,
+                style = MaterialTheme.typography.titleLarge,
                 color = VideoDownloaderTheme.colors.black
             )
         }, actions = {
             Icon(
-                modifier = Modifier.size(24.dp),
-                painter = painterResource(R.drawable.ic_setting),
-                contentDescription = null
+                modifier = Modifier.size(18.dp),
+                painter = painterResource(com.mirdar.designsystem.R.drawable.ic_setting_fill),
+                contentDescription = null,
+                tint = VideoDownloaderTheme.colors.black
             )
+            Spacer(Modifier.width(8.dp))
         })
 }
