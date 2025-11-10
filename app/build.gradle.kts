063ba3ca259bf26424306f7ff0b200bfd3e781eb
Mohammad Mirdar <mohammadmirdarmpanahi@gmail.com>
2025-11-10 09:23:11 +0330

    increase app version 140408191

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,10 +19,10 @@ android {
 
     defaultConfig {
         applicationId = "com.mirdar.videodownloader"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 2
-        versionName = "1.0"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
 
         buildConfigField(
             "String",
